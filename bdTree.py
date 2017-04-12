728ec161d7b691e4035397e60071f753bf855dbd
Jordan <jordan@localhost.localdomain>
2017-04-12 18:54:59 +0100

    Added printing

--- bdTree.py
+++ bdTree.py
@@ -1,3 +1,5 @@
+from asciitree import draw_tree
+
 class node:
     "Divisor Tree Node"
     def __init__(self,n):
@@ -8,17 +10,36 @@ class node:
     def addChild(self,m):
         "Add new node, or recursive call to next child node"
         if self.n == m : return
-        if self.m%n == 0:
+        if m % self.n == 0:
             if not self.div: self.div = node(m)
             else: self.div.addChild(m)
         else:
             if not self.prm: self.prm = node(m)
             else: self.prm.addChild(m)
 
+def getNodeChildren(node):
+    "Return iterable of a nodes children"
+    ret = []
+    if node.prm: ret.append(node.prm)
+    if node.div: ret.append(node.div)
+    return ret
+
+def getNodeString(node):
+    "Return nodes value as str"
+    return str(node.n)
+
 class divTree:
     "Divisor tree container"
     def __init__(self,i):
         self.root = node(i)
     
-    def addNode(n):
+    def addNode(self,n):
         self.root.addChild(n)
+
+    def printTree(self):
+        print draw_tree(self.root,getNodeChildren,getNodeString)
+
+A = divTree(2)
+for i in range(3,8): A.addNode(i)
+
+A.printTree()
